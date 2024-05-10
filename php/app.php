6a995e201ee4341e3cd30a7e4384e5e230ea3550
SaadBarhrouj <saad.barhrouj@etu.uae.ac.ma>
2024-05-10 17:24:05 -0400

    update

---
+++ php/app.php
@@ -0,0 +1,125 @@
+<?php
+    // Inclure le fichier de connexion à la base de données
+    include "connexion.php";
+
+    // Démarrer la session pour récupérer l'email de l'utilisateur connecté
+    session_start();
+    $email = $_SESSION['email'];
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Résultats</title>
+    <style>
+       h1 {
+            margin: 0;
+            margin-bottom: 80px;
+            padding: 100px 0; /* Ajout d'un espace en haut et en bas du titre */
+            font-size: 5em;
+            width: 100%;
+            color: white;
+            text-align: center;
+            background-image: url("images/image2.jpeg");
+            background-size: 100% auto; /* Utiliser la largeur de l'image originale */
+            background-position: top center; /* Positionner l'image en haut et centrer horizontalement */
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.7);
+        }
+
+        body {
+            width: 80%;
+            margin: auto;
+        }
+
+        table {
+            margin-top: 20px;
+            border-collapse: collapse;
+            margin: 25px auto;
+            padding: 5px;
+            font-size: 0.9em;
+            font-family: sans-serif;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+            width: 100%;
+            background-color: #6d9fc5; /* Bleu moyen pour le tableau */
+            color: #fff; /* Texte blanc dans le tableau */
+        }
+
+        td, th {
+            padding: 15px;
+            text-align: center;
+            font-size: 1.5em;
+        }
+
+        thead tr{
+            background-color : black;
+            color : #ffffff;
+            text-align : center;
+        }
+
+        tbody tr {
+            border-bottom: 1px solid #dddddd;
+        }
+
+        tbody tr.active-row {
+            font-weight: bold;
+            color: #009879;
+        }
+
+        tbody tr:nth-of-type(even) {
+            background-color: lightgrey;
+            color: black;
+        }
+        tbody tr:nth-of-type(odd) {
+            background-color: white; 
+            color: black;
+        }
+    </style>
+</head>
+<body>
+
+<h1>Résultats</h1>
+
+<table>
+    <thead>
+    <tr>
+        <th>Matière</th>
+        <th>Note</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    // Préparer et exécuter la requête SQL pour récupérer les résultats de l'étudiant
+    $sql = "SELECT * FROM student WHERE email = '$email'";
+    $result = mysqli_query($link, $sql);
+
+    // Afficher les résultats dans le tableau
+    while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+        <tr>
+            <td>SE</td>
+            <td><?php echo $row["système d'exploitation"]; ?></td>
+        </tr>
+        <tr>
+            <td>C Avancé</td>
+            <td><?php echo $row["C Avancé"]; ?></td>
+        </tr>
+        <tr>
+            <td>ADO</td>
+            <td><?php echo $row["ADO"]; ?></td>
+        </tr>
+        <tr>
+            <td>THL</td>
+            <td><?php echo $row["THL"]; ?></td>
+        </tr>
+        <tr>
+            <td>Développement Web</td>
+            <td><?php echo $row["Développement web"]; ?></td>
+        </tr>
+        <?php
+    }
+    ?>
+    </tbody>
+</table>
+
+</body>
+</html>
---
+++ php/index.php
@@ -0,0 +1,168 @@
+<?php
+// Vérifier si le formulaire a été soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Connexion à la base de données
+    include "connexion.php";
+
+    // Vérifier si la connexion a été établie avec succès
+    if (!$link) {
+        die("La connexion à la base de données a échoué: " . mysqli_connect_error());
+    }
+
+    // Récupérer les données du formulaire
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Vérifier les informations de connexion dans la base de données
+    $sql = "SELECT * FROM student WHERE email = '$email' AND password = '$password'";
+    $result = mysqli_query($link,$sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        // L'utilisateur est authentifié, démarrer une session et stocker l'e-mail
+        session_start();
+        $_SESSION['email'] = $email;
+        // Rediriger vers la page des notes
+        header('Location: app.php');
+        exit; // Terminer le script après la redirection
+    } else {
+        // Rediriger vers la page de connexion avec un message d'erreur
+        header('Location: login.php?error=1');
+        exit; // Terminer le script après la redirection
+    }
+
+    // Fermer la connexion à la base de données
+    mysqli_close($link);
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"/>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+        }
+
+        .home {
+            position: relative;
+            height: 100vh;
+            width: 100%;
+            background-image: url("images/image1.jpg");
+            background-position: center;
+            background-size: cover;
+        }
+
+        .login_form {
+            box-size: 80px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background: rgba(255, 255, 255, 0.8);
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+
+        }
+
+        .login_form h2 {
+            text-align: center;
+            margin-top: 10px;
+            margin-bottom: 30px;
+        }
+
+        .input_box {
+            position: relative;
+            margin-bottom: 30px;
+        }
+
+        .input_box input {
+            width: 100%;
+            padding: 10px;
+            border: none;
+            border-bottom: 2px solid #333;
+            outline: none;
+            font-size: 16px;
+        }
+
+        .input_box input:focus {
+            border-bottom: 1px solid #007bff; /* Couleur bleue pour mettre en évidence le champ actif */
+        }
+
+        .input_box i {
+            position: absolute;
+            top: 50%;
+            right: 10px;
+            transform: translateY(-50%);
+            color: #333;
+        }
+
+        .pw-hide {
+            cursor: pointer;
+        }
+
+        button[type="submit"] {
+            display: block;
+            width: 100%;
+            padding: 10px;
+            margin-top: 15px;
+            background-color: #007bff; /* Couleur bleue */
+            color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+            transition: background-color 0.3s ease;
+        }
+
+        button[type="submit"]:hover {
+            background-color: #0056b3; /* Couleur bleue plus foncée au survol */
+        }
+
+        .error-message {
+            text-align: center;
+            color: red;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+    <section class="home"></section>
+    <div class="login_form">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+            <h2>Login</h2>
+            <?php if(isset($_GET['error']) && $_GET['error'] == 1): ?>
+                <p class="error-message">Adresse e-mail ou mot de passe incorrect.</p>
+            <?php endif; ?>
+            <div class="input_box">
+                <input type="email" name="email" placeholder="Adresse e-mail" required>
+                <i class="uil uil-envelope-alt email"></i>
+            </div>
+            <div class="input_box">
+                <input type="password" name="password" id="password" placeholder="Mot de passe" required />
+                <i class="uil uil-lock password"></i>
+                <i class="uil uil-eye-slash pw-hide" id="togglePassword"></i>
+            </div>
+            <button type="submit">Se connecter</button>
+        </form>
+    </div>
+
+    <script>
+        // JavaScript pour afficher/masquer le mot de passe
+        const togglePassword = document.getElementById('togglePassword');
+        const password = document.getElementById('password');
+
+        togglePassword.addEventListener('click', function () {
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+            this.classList.toggle('uil-eye-slash');
+        });
+    </script>
+</body>
+</html>
